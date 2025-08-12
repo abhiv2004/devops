8f0aad381c340e37e01cbd1971e48b3b6b8a151e
AbhishekKumar Varma <118065970+abhiv2004@users.noreply.github.com>
2025-08-12 09:16:58 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo{
+  public static void main(){
+      int n = 5;
+      for(int i = 0 ; i < n ; i++){
+          for(int j =0 ; j < n ; j++){
+            System.out.print("*");
+      }
+      System.out.println();
+  }
+}
