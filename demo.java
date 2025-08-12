8b716a175b81d79d26324246a990d58fbea0e845
AbhishekKumar Varma <118065970+abhiv2004@users.noreply.github.com>
2025-08-12 09:19:25 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -4,7 +4,8 @@ public class demo{
       for(int i = 0 ; i < n ; i++){
           for(int j =0 ; j < n ; j++){
             System.out.print("*");
-      }
+        }
       System.out.println();
+      }
   }
 }
