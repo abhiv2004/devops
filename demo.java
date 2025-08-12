21993af4a8630adcca836d79d2db33ce053aa41e
AbhishekKumar Varma <118065970+abhiv2004@users.noreply.github.com>
2025-08-12 09:27:59 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo{
-  public static void main(){
+  public static void main(String [] args){
       int n = 5;
       for(int i = 0 ; i < n ; i++){
           for(int j =0 ; j < n ; j++){
